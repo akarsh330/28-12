15778728678b440a7918e6466ffa383a546ba3c8
akarsh330 <akarshgowda7711@gmail.com>
2024-12-28 10:36:25 +0530

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+for x in range(3):
+    for y in range(10):
+        print("*")
+
+
+for x in range(3):
+    for y in range(10):
+        print(x,y)
+
+
+
+
+for x in range(3):
+    for y in range(3):
+        print("*",end='')
+
+
